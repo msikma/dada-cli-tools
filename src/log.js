fbc57ceae10f72dc539f85913cf6459d3bc74d93
Michiel Sikma <michiel@sikma.org>
2019-09-20 01:35:52 +0200

    Implement better logging verbosity

--- src/log.js
+++ src/log.js
@@ -3,20 +3,40 @@
 
 import chalk from 'chalk'
 import { inspect } from 'util'
+import { isNumber, isString } from 'lodash'
 
 import { progName } from './util'
 
-// Placeholder for the user's CLI arguments. These determine
-// the verbosity of logging, among other things.
-let args = {}
+// Shortcut labels used to describe various verbosity levels.
+const verbosityLabels = {
+  error: 8,
+  warn: 6,
+  info: 4,
+  verbose: 2,
+  debug: 0,
+  quiet: -1
+}
+
+// Contains the verbosity setting that determines how much gets logged.
+// The value should always be a number, with 0 meaning no logging.
+const options = {
+  // Log 'info' and up by default.
+  verbose: verbosityLabels['info']
+}
 
 // Regex used to colorize certain log patterns.
 const HTTP_PROTOCOL = new RegExp('^\s?https?://')
 const ABS_REL_PATH = new RegExp('^\s?\.?/')
 
-/** Saves a copy of the user's CLI arguments for use in these functions. */
-export const setParams = (params) => {
-  args = { ...args, ...params }
+/** Sets the desired logging verbosity. */
+export const setVerbosity = (verbosity) => {
+  if (isNumber(verbosity)) {
+    options.verbose = verbosity
+    return
+  }
+  const vbNumber = verbosityLabels[verbosity]
+  if (vbNumber == null) throw new Error(`setVerbosity() called with unrecognized label (${verbosity})`)
+  options.verbose = vbNumber
 }
 
 /** Logs strings and objects to the console. */
@@ -40,17 +60,26 @@ const logSegments = (segments, logFn = console.log, colorize = true, colorAll = 
   logFn(colorAll ? colorAll(str) : str)
 }
 
-/** Logs a line of text if verbose (-v) logging is enabled. */
+/** Logs a line of text with standard verbosity (0). */
 export const log = (...segments) => {
-  if (args.verbose && args.isCli) {
-    logSegments(segments)
+  if (options.verbosity < 0) return
+  logSegments(segments)
+}
+
+/**
+ * Logs a line of text with a given verbosity value.
+ * The higher the verbosity value, the less critical the log is.
+ */
+export const logVerbose = (verbosity, segments) => {
+  if (!isNumber(verbosity) || !Array.isArray(segments)) {
+    throw new Error('logVerbose() must be called with interface: verbosity:Number, segments:Array')
   }
+  if (options.verbosity < verbosity) return
+  logSegments(segments)
 }
 
 /** Exits the program with an error. */
 export const die = (...segments) => {
-  if (args.isCli) {
-    logSegments([`${progName()}:`, ...segments], console.error, false, chalk.red)
-  }
+  logSegments([`${progName()}:`, ...segments], console.error, false, chalk.red)
   process.exit(1)
 }
