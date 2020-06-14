84756b4975cb32f41728b3e4f0f653dad8e9b90e
Michiel Sikma <michiel@sikma.org>
2020-06-14 16:19:15 +0200

    Fix broken export statement for util/index and fix broken Jest setup

--- src/util/index.js
+++ src/util/index.js
@@ -1,18 +1,28 @@
 // dada-cli-tools - Libraries for making CLI programs <https://github.com/msikma/dada-cli-tools>
 // © MIT license
 
+import * as error from './error'
 import * as fs from './fs'
+import * as html from './html'
 import * as misc from './misc'
 import * as output from './output'
+import * as promise from './promise'
 import * as query from './query'
-import * as read from './read'
+import * as slug from './slug'
 import * as text from './text'
+import * as vm from './vm'
+import * as xml from './xml'
 
 export {
+  error,
   fs,
+  html,
   misc,
   output,
+  promise,
   query,
-  read,
-  text
+  slug,
+  text,
+  vm,
+  xml
 }
--- util/index.js
+++ util/index.js
@@ -3,11 +3,19 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.text = exports.read = exports.query = exports.output = exports.misc = exports.fs = void 0;
+exports.xml = exports.vm = exports.text = exports.slug = exports.query = exports.promise = exports.output = exports.misc = exports.html = exports.fs = exports.error = void 0;
+
+var error = _interopRequireWildcard(require("./error"));
+
+exports.error = error;
 
 var fs = _interopRequireWildcard(require("./fs"));
 
 exports.fs = fs;
+
+var html = _interopRequireWildcard(require("./html"));
+
+exports.html = html;
 
 var misc = _interopRequireWildcard(require("./misc"));
 
@@ -17,16 +25,28 @@ var output = _interopRequireWildcard(require("./output"));
 
 exports.output = output;
 
+var promise = _interopRequireWildcard(require("./promise"));
+
+exports.promise = promise;
+
 var query = _interopRequireWildcard(require("./query"));
 
 exports.query = query;
 
-var read = _interopRequireWildcard(require("./read"));
+var slug = _interopRequireWildcard(require("./slug"));
 
-exports.read = read;
+exports.slug = slug;
 
 var text = _interopRequireWildcard(require("./text"));
 
 exports.text = text;
+
+var vm = _interopRequireWildcard(require("./vm"));
+
+exports.vm = vm;
+
+var xml = _interopRequireWildcard(require("./xml"));
+
+exports.xml = xml;
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
